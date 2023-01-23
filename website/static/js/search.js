ba8b00e9aeb66c81c9e25a95ba3c957871d650ef
Maximoace <maximoarthuro@gmail.com>
2023-01-23 17:22:01 -0300

    Added static components to website; Added vital search.js functionalities

---
+++ website/static/js/search.js
@@ -0,0 +1,31 @@
+const dataRowTemplate = document.querySelector("[data-row-template]")
+const dataContainer = document.querySelector("[data-container]")
+const searchInput = document.querySelector("[data-search]")
+
+let deputies = []
+
+searchInput.addEventListener("input", (e) =>{
+    const value = e.target.value.toLowerCase()
+    deputies.forEach(deputy => {
+        const isVisible = deputy.name.toLowerCase().includes(value)
+        deputy.element.classList.toggle("hide", !isVisible)
+    })
+})
+
+fetch('/fetchTable')
+.then(res => res.json())
+.then(data => {
+    deputies = data.map(deputy =>{
+        const row = dataRowTemplate.content.cloneNode(true).children[0]
+        const name = row.querySelector("[data-name]")
+        const party = row.querySelector("[data-party]")
+        const state = row.querySelector("[data-state]")
+        const status = row.querySelector("[data-status]")
+        name.textContent = deputy.name
+        party.textContent = deputy.party
+        state.textContent = deputy.state
+        status.textContent = deputy.status
+        dataContainer.append(row)
+        return {name: deputy.name, element: row}
+    })
+})
