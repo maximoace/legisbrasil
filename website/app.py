f8cd7968702f464aac01bcaa1a92b2bcc15d3fdb
Maximoace <maximoarthuro@gmail.com>
2023-01-23 17:23:07 -0300

    Updated routing for website manipulation; Added search page route; Added basic info api call

--- website/app.py
+++ website/app.py
@@ -1,8 +1,24 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, jsonify
+
+from legisbrasil.database.database import Database
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def home():
     return render_template('home.html')
+
+@app.route("/search")
+def search():
+    return render_template('search.html')
+
+@app.route("/fetchTable")
+def fetchTable():
+    data = Database().read_basic()
+    print(data)
+    return data
+
+@app.route("/details/<id>")
+def details():
+    pass
