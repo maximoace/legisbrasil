f64e7eac2d5d0f9f131ad213034272a11ff7fdd8
Maximoace <maximoarthuro@gmail.com>
2023-01-23 23:53:10 -0300

    Added detailed info webscrapping and database search; program will now try 3 times before jumping to next id.

--- database/database.py
+++ database/database.py
@@ -1,24 +1,54 @@
 import pandas as pd
 import sqlite3
+import json
 import os
 
 class Database():
+
+    ## General purpose MySQL functions
 
     def __init__(self) -> None:
         self.con = None
         self.cur = None
 
     def connect(self):
-        self.con = sqlite3.connect('database/deputies.db')
+        self.con = sqlite3.connect('database/db.sqlite')
 
     def close(self):
         self.con.close()
         self.con = None
 
     def exists(self):
-        return os.path.exists('database/deputies.db')
+        return os.path.exists('database/db.sqlite')
+
+    ## End of general purposes functionss
 
     def save(self, data):
         self.connect()
         dataframe = pd.DataFrame.from_dict(data)
         dataframe.to_sql('deputies', self.con, if_exists='replace', index=False)
+        self.close()
+
+    def read_basic(self):
+        self.connect()
+        dataframe = pd.read_sql("SELECT id, name, party, state, status FROM deputies", self.con)
+        self.close()
+
+        ## Data needs some sanitizing before utilizing, as it comes in a [{'columns':[...], 'data':[[...], [...], ...]}]
+        ## Needs to return a list of dicts with column, value pair.
+
+        deputies = list()
+
+        json_string = dataframe.to_json(orient='split', index=False, force_ascii=False)
+        json_item = json.loads(json_string)
+        for row in json_item['data']:
+            dict_item = dict(zip(json_item['columns'], row))
+            deputies.append(dict_item)
+        return deputies
+
+    def read_detailed(self, id):
+        self.connect()
+        dataframe = pd.read_sql(f"SELECT * FROM deputies WHERE id = ?", self.con, params=[id])
+        self.close()
+        data = dataframe.to_dict(orient='index')[0]
+        return data
--- webscrapper/webscrapper.py
+++ webscrapper/webscrapper.py
@@ -16,8 +16,17 @@ class WebScrapper():
 
     def get_data(self):
         self.webdriver.start()
-        self.webdriver.connect("https://www.camara.leg.br/deputados/quem-sao/resultado?legislatura=56")
-        self.collect_basic_data()
+        attempt = 0
+        while attempt < 3:
+            try:
+                self.webdriver.connect("https://www.camara.leg.br/deputados/quem-sao/resultado?legislatura=56")
+                self.collect_basic_data()
+                break
+            except:
+                attempt +=1
+                print(f"Failed {attempt} attempt at gathering general basic data")
+        self.collect_detailed_data()
+        print("Dados dos deputados obtidos com sucesso.")
         self.webdriver.finish()
 
     def save_data(self):
@@ -26,6 +35,7 @@ class WebScrapper():
     def collect_basic_data(self):
         data = list()
         while True:
+
             page_deputies = self.webdriver.select_all_by_class('lista-resultados__txt')
             for deputy_info in page_deputies:
                 deputy = dict()
@@ -34,7 +44,6 @@ class WebScrapper():
                 self.collect_basic_info(deputy, soup)
                 
                 data.append(deputy)
-
             next_page = self.webdriver.find_by_class('pagination-list__nav--next')
             if 'disabled' not in self.webdriver.get_attr('class', next_page):
                 self.webdriver.click(next_page)
@@ -53,3 +62,79 @@ class WebScrapper():
         deputy['name'] = info[0:info.find(" (")]
         deputy['party'], deputy['state'] = party_state.split('-')
         deputy['status'] = soup.find('span').get_text(strip=True)
+
+    def collect_detailed_data(self):
+        base_url = "https://www.camara.leg.br/deputados"
+
+        for deputy in self.data:
+            self.collect_summary_page(deputy, base_url)
+            self.collect_biography(deputy, base_url)
+
+    def collect_summary_page(self, deputy:dict, base_url):
+        attempt = 0
+        while attempt < 3:
+            try:
+                indexed_url = f"{base_url}/{deputy['id']}"
+                self.webdriver.connect(indexed_url)
+
+                info_section = self.webdriver.find_by_class("informacoes-deputado")
+                soup = BeautifulSoup(self.webdriver.get_attr("outerHTML", info_section), 'html.parser')
+                self.collect_summary_info(deputy, soup)
+                break
+            except:
+                attempt += 1
+                print(f"Failed {attempt} attempt(s) at obtaining ID:{deputy['id']} summary")
+
+    def collect_summary_info(self, deputy:dict, soup):
+        info_list = soup.find_all('li')
+
+        for info in info_list:
+            text = info.get_text()
+            if 'Nome' in text:
+                deputy["fullname"] = text[text.find(': ')+2:]
+            elif 'E-mail' in text:
+                deputy["email"] = text[text.find(': ')+2:]
+            elif 'Telefone' in text:
+                deputy["phone"] = text[text.find(': ')+2:]
+            elif 'Endereço' in text:
+                deputy["address"] = text[text.find(': ')+2:].replace('\n', '').replace('   ', '')
+            elif 'Nascimento' in text:
+                deputy["birthday"] = text[text.find(': ')+2:]
+            elif 'Naturalidade' in text:
+                deputy["pob"] = text[text.find(':')+1:].replace('\n','').replace('  ','')
+        
+    def collect_biography(self, deputy:dict, base_url):
+        attempt = 0
+        while attempt < 3:
+            try:
+                biography_url = f"{base_url}/{deputy['id']}/biografia"
+                self.webdriver.connect(biography_url)
+
+                info_section = self.webdriver.find_by_class("informacoes-deputado")
+                soup = BeautifulSoup(self.webdriver.get_attr("outerHTML", info_section), 'html.parser')
+                self.collect_biography_info(deputy, soup)
+
+                info_section = self.webdriver.find_by_class("biografia-detalhes-deputado")
+                soup = BeautifulSoup(self.webdriver.get_attr("outerHTML", info_section), 'html.parser')
+                self.collect_biography_extra(deputy, soup)
+                break
+            except:
+                attempt += 1
+                print(f"Failed {attempt} attempt(s) at obtaining ID:{deputy['id']} biography")
+
+
+    def collect_biography_info(self, deputy:dict, soup):
+        info_list = soup.find_all('li')
+        for info in info_list:
+            text = info.get_text()
+            if 'Profissões' in text:
+                deputy['jobs'] = text[text.find(':')+2:]
+            if 'Escolaridade' in text:
+                deputy['scholarity'] = text[text.find(':')+2:]
+
+    def collect_biography_extra(self, deputy:dict, soup):
+        info_name_list = soup.find_all('strong')
+        for name in info_name_list:
+            text = name.get_text()
+            if 'Mandatos' in text:
+                deputy['mandate'] = name.next_sibling.next_sibling.get_text()[1:]
